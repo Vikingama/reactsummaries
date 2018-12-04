1e11cdda96556985ae6a51c789cfe6c4e8f579d5
VikingX <Pavilion0321@gmail.com>
2018-12-04 14:17:28 +0800

    hahasha

--- mobs/src/App.js
+++ mobs/src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class App extends Component {
     render () {
         return (
-            <div className="App"></div>
+            <div className="App">看完 MOBX 还是一头雾水，加油...</div>
         );
     }
 }
