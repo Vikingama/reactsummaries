d95bcf54cc06bf8f779aad9b4b821a1778635a9a
Vikingama <pavilion0321@gmail.com>
2019-03-29 15:11:18 +0800

    hahasha

---
+++ watch/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Watch from "./Watch";
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Watch />
+            </div>
+        );
+    }
+}
+
+export default App;
